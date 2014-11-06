5bf921cadfac21b9a95a200d759f34a4d6668215
zhuwowuyuing <zhuwowuyuing@gmail.com>
2014-11-06 11:03:29 +0800

    修正查看资产信息无法，baseinfo 错误

---
+++ deploy/local_wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for Deploy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os, sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deploy.local_settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
